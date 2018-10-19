e6771373d97a4dca6ec884bd5b50ad4f625137bc
Apple <apple@apples-mbp.gateway.sasktel.net>
2018-10-18 21:42:23 -0600

    made first get query

---
+++ schema.js
@@ -0,0 +1,48 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull,
+} = require('graphql');
+
+const customers = [
+    {id: '1', name: 'Edgar Khimich', email: 'ed@gmail.com', age: 25}, 
+    {id: '2', name: 'John Khimich', email: 'john@gmail.com', age: 41}, 
+    {id: '3', name: 'Erik Khimich', email: 'erik@gmail.com', age: 12}, 
+];
+
+
+const CustomerType = new GraphQLObjectType({
+    name: 'Customer',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt},
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parentValue, args){
+                for(let i = 0; i<customers.length; i++){
+                    if(customers[i].id == args.id){
+                        return customers[i];
+                    }
+                }
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
